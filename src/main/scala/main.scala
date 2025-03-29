560bb391a9e816dd13dae76278ee76921fb7a06e
Lesaje <olehchaika.prop@gmail.com>
2025-03-29 14:17:53 +0200

    works with basic info; v.0.1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,27 +1,6 @@
-import task2.{Course, Scraper}
-import task2.Scraper.{extractCourseName, extractShortDescription, getCourseElements, setupDriver}
+import task2.Scraper
 
 @main
 def main(): Unit = {
-  val driver = setupDriver()
-
-  try {
-    driver.get("https://mate.academy/")
-
-    val courseElements = getCourseElements(driver)
-
-    val coursesInfo: List[Course] = courseElements.map { courseElem =>
-      val name = extractCourseName(courseElem)
-      val description = extractShortDescription(courseElem)
-      Course(name, description)
-    }
-
-    coursesInfo.foreach { course =>
-      println(s"Course Name: ${course.name}")
-      println(s"Short Description: ${course.shortDescription}")
-      println("--------------")
-    }
-  } finally {
-    driver.quit()
-  }
+  Scraper.scrapeCourses()
 }
--- src/main/scala/task2/Scraper.scala
+++ src/main/scala/task2/Scraper.scala
@@ -1,11 +1,10 @@
 package task2
 
-import org.openqa.selenium.{By, WebDriver, WebElement}
+import org.openqa.selenium.{By, JavascriptExecutor, WebDriver, WebElement}
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
-import org.openqa.selenium.interactions.Actions
 import org.openqa.selenium.support.ui.{ExpectedConditions, WebDriverWait}
-import scala.jdk.CollectionConverters._
 
+import scala.jdk.CollectionConverters.*
 import java.time.Duration
 
 object Scraper {
@@ -24,16 +23,54 @@ object Scraper {
     wait.until(ExpectedConditions.presenceOfElementLocated(by))
   }
 
+  def scrollIntoView(driver: WebDriver, element: WebElement): Unit = {
+    driver.asInstanceOf[JavascriptExecutor].executeScript("arguments[0].scrollIntoView(true);", element)
+  }
+
   def getCourseElements(driver: WebDriver): List[WebElement] = {
     waitForElementPresence(driver, By.cssSelector("a.ProfessionCard_cardWrapper__BCg0O"))
     driver.findElements(By.cssSelector("a.ProfessionCard_cardWrapper__BCg0O")).asScala.toList
   }
 
-  def extractCourseName(courseElement: WebElement): String = {
-    courseElement.findElement(By.cssSelector("h3.ProfessionCard_title__m7uno")).getText.trim
+  def extractCourseName(driver: WebDriver, courseElement: WebElement): String = {
+    scrollIntoView(driver, courseElement)
+    val titleElement = courseElement.findElement(By.cssSelector("h3.ProfessionCard_title__m7uno"))
+    titleElement.getAttribute("textContent").trim
   }
 
-  def extractShortDescription(courseElement: WebElement): String = {
-    courseElement.findElement(By.cssSelector("p.ProfessionCard_description__K8weo")).getText.trim
+  def extractShortDescription(driver: WebDriver, courseElement: WebElement): String = {
+    scrollIntoView(driver, courseElement)
+    val descElement = courseElement.findElement(By.cssSelector("p.ProfessionCard_description__K8weo"))
+    descElement.getAttribute("textContent").trim
+  }
+
+  def extractDuration(driver: WebDriver, courseElement: WebElement): String = {
+    scrollIntoView(driver, courseElement)
+    val durationElement = courseElement.findElement(By.cssSelector("p.ProfessionCard_duration__13PwX"))
+    durationElement.getAttribute("textContent").trim
+  }
+
+  def scrapeCourses(): Unit = {
+    val driver = setupDriver()
+
+    try {
+      driver.get("https://mate.academy/")
+
+      val courseElements = getCourseElements(driver)
+      println(s"Found ${courseElements.size} course cards.")
+
+      courseElements.zipWithIndex.foreach { case (element, idx) =>
+        val name = extractCourseName(driver, element)
+        val shortDescription = extractShortDescription(driver, element)
+        val duration = extractDuration(driver, element)
+        println(s"Course #${idx + 1}: $name")
+        println(s"Duration: $duration")
+        println(s"Short Description: $shortDescription")
+        println("--------------")
+      }
+
+    } finally {
+      driver.quit()
+    }
   }
 }
